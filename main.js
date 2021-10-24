4cca71cadb24e8846e26169e2133d0e092eb9435
Marcela Leoni <91268490+marcelaleoni@users.noreply.github.com>
2021-10-24 13:25:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const linksSocialMedia = {
+  github: 'marcelaleoni',
+  youtube: 'marcyandluke',
+  instagram: 'nerdmarcy',
+  twitter: 'marcyandluke'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+// i = i + 1
+// i++
+
+//ser humano 12345678910
+//computador 0123456789
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
